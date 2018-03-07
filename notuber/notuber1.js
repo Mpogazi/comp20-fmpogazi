f9b4f7e1bb4d8417f5d60dbdd9f6fac67520e8f9
unknown <Fabrice.Mpogazi@tufts.edu>
2018-03-07 02:21:38 -0500

    Changed the index file and separated it from the Javascript

---
+++ notuber/notuber1.js
@@ -0,0 +1,80 @@
+var myLat = 0;
+var myLng = 0;
+var myLocation = new google.maps.LatLng(myLat, myLng);
+var options = {
+    zoom: 12,
+    center: myLocation,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+var map;
+var hxr;
+var infowindow = new google.maps.InfoWindow();
+var distanceFromMe = [];
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), options);
+    getMyLocation();
+}
+
+function getMyLocation() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            myLat = position.coords.latitude;
+            myLng = position.coords.longitude;
+            requestData();
+            renderMap();
+        });
+    }
+}
+
+function renderMap() {
+    myLocation = new google.maps.LatLng(myLat, myLng);
+    map.panTo(myLocation);
+    var meIcon;
+    meIcon = "rick_pass.png";
+    console.log(distanceFromMe);
+    makeMarker(myLocation, meIcon, map, Math.min(...distanceFromMe), "DsRILKPCEf");
+}
+
+function requestData() {
+    hxr = new XMLHttpRequest;
+    var url = "https://jordan-marsh.herokuapp.com/rides";
+    var params = "username=DsRILKPCEf&lat="+myLat+"&lng="+myLng;
+    
+    hxr.open('POST', url, true);
+    
+    hxr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    
+    hxr.onreadystatechange = function() {
+        if(hxr.readyState == 4 && hxr.status == 200) {
+            var message = JSON.parse(hxr.responseText);
+            var key = Object.keys(message)[0];
+            var arrayM = message[key];
+            var icons = "driver_icon.png";
+            if(arrayM == 'passengers') {
+                icons = "jumbo_passenger.png";
+            }
+            for(var i = 0; i < arrayM.length; i++) {
+                var folkLocation = new google.maps.LatLng(arrayM[i].lat, arrayM[i].lng);
+                var distancePerson = google.maps.geometry.spherical.computeDistanceBetween(folkLocation, myLocation) * 0.000621371;
+                distanceFromMe.push(distancePerson);
+                makeMarker(folkLocation, icons, map, distancePerson, arrayM[i].username);                
+            }
+        }
+    }
+    hxr.send(params);
+    
+}
+
+function makeMarker(place, icon, map, distance, uname) {
+    var markerM = new google.maps.Marker({
+        position: place,
+        icon: icon,
+        title: "username: "+uname+" </br>"+distance+"  miles from Me"
+    });
+    markerM.setMap(map);
+    google.maps.event.addListener(markerM, 'click', function() {
+        infowindow.setContent(markerM.title);
+        infowindow.open(map, markerM);
+    });
+}
